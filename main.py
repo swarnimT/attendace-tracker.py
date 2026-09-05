de6ceb0840cb89474265b35d0af094fd9fe52d07
swarnimT <thakreswarnim1@gmail.com>
2026-09-05 13:47:23 +0000

    Sep 5, 2026, 7:17 PM

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import gchh
+import sqlite3
+import os
+from datetime import datetime
+from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment
+from openpyxl.utils import get_column_letter
+
+DB_NAME = "attendance.db"
+REPORT_FOLDER = "reports"
+
+os.makedirs(REPORT_FOLDER, exist_ok=True)
+
+connection = sqlite3.connect(DB_NAME)
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (student_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, creation_date TEXT NOT NULL)""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS attendance (attendance_id INTEGER PRIMARY KEY AUTOINCREMENT, student_id INTEGER, date TEXT NOT NULL, status TEXT NOT NULL, FOREIGN KEY (student_id) REFERENCES students (student_id))""")
