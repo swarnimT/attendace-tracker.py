0262fa7f7d12cb8b0195a4d9bf82293cd39c55c4
swarnimT <thakreswarnim1@gmail.com>
2026-09-05 14:15:26 +0000

    Sep 5, 2026, 7:45 PM

--- main.py
+++ main.py
@@ -29,5 +29,42 @@ def get_students():
     return cursor.fetchall()
 
 def get_subjects():
-    cursor.execute("SELECT * FROM subjects WHERE student_id=?", (subject_id,))
+    cursor.execute("SELECT * FROM subjects WHERE student_id=?", (student_id,))
     return cursor.fetchall()
+
+
+def clean_filename(name):
+    return "".join(c for c in name if c.isalnum() or c in (" ", "_", "-")).strip().replace(" ", "_")
+
+def create_student():
+    print("\n Create Student")
+    name=input("Enter student name: ").strip()
+    if not name:
+        print("Student name cannot be empty.")
+        return
+    creation_date = input("Enter creation date (DD-MM-YYYY): ").strip()
+    try:
+        datetime.strptime(creation_date, "%d-%m-%Y")
+    except ValueError:
+        print("Invalid date format. Please use DD-MM-YYYY.")
+        return
+    cursor.execute("""INSERT INTO students (name, creation_date) VALUES (?, ?)""", (name, creation_date))
+    student_id = cursor.lastrowid
+    connection.commit()
+    print("/nHow many subjects does the you have?")
+
+    try:
+        number_of_subjects = int(input("Enter number of subjects: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return
+    if number_of_subjects <= 0:
+        print("you need atleast one subject.")
+        return
+    print("\nAdd subjects")
+    for i in range(number_of_subjects):
+        subject_name = input(f"Enter subject {i + 1}: ").strip()
+        if not subject_name:
+            print("Subject name cannot be empty.")
+            return
+        cursor.execute("""INSERT INTO subjects (student_id, name) VALUES (?, ?)""", (student_id, subject_name))
