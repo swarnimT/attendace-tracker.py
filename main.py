74530cc4382587c6249c01485be2bc9884491d88
swarnimT <thakreswarnim1@gmail.com>
2026-09-05 13:57:11 +0000

    Sep 5, 2026, 7:27 PM

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ cursor.execute("""
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS attendance (attendance_id INTEGER PRIMARY KEY AUTOINCREMENT, student_id INTEGER NOT NULL,student_id INTEGER NOT NULL, date TEXT NOT NULL, status TEXT NOT NULL, FOREIGN KEY (student_id) REFERENCES students (student_id),FOREIGN KEY (student_id) REFERENCES students (student_id))""")
 
-sqlite3.Cursor.execute("""
+cursor.execute("""
     CREATE TABLE IF NOT EXISTS lecture_summary (summary_id INTEGER PRIMARY KEY AUTOINCREMENT,student_id INTEGER NOT NULL, student_id INTEGER NOT NULL,total_lectures INTEGER NOT NULL,attended_lectures INTEGER NOT NULL, FOREIGN KEY (student_id) REFERENCES students (student_id),FOREIGN KEY (subject_id) REFERENCES subjects (subject_id))""")
